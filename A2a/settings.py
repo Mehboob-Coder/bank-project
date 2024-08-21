34521be95ac3b2ce5edd8a035b7fbf852b2c6b8f
Mehboob-Coder <ahma41980@gmail.com>
2024-08-21 10:03:21 +0500

    mehboob

--- A2a/settings.py
+++ A2a/settings.py
@@ -124,10 +124,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT =587
-EMAIL_USE_TLS= True
-EMAIL_HOST_USER= 'djangotesting5@gmail.com'
-EMAIL_HOST_PASSWORD = 'vkehfsazzoquntlw'
+from Account.env import *
+EMAIL_BACKEND = EMAIL_BACKEND 
+EMAIL_HOST = EMAIL_HOST
+EMAIL_PORT =EMAIL_PORT
+EMAIL_USE_TLS= EMAIL_USE_TLS
+EMAIL_HOST_USER= EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
